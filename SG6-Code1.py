b2584fd27ceb5032880d8721c410a44861016ba3
HerlieVinceBDiente <herlievince.diente@smc.pshs.edu.ph>
2025-09-10 09:03:20 +0800

    SG6-CODE 1
    My code for code 1

---
+++ SG6-Code1.py
@@ -0,0 +1,7 @@
+x = float(input("Enter the frst number: "))
+y = float(input("Enter the secondd number: "))
+z = float(input("Enter the third number: "))
+
+boglat = x+y+z
+bonak = round(boglat, 2)
+print("The sum of the three numbers is ", bonak)
